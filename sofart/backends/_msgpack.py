c958618d158807026e441fb57731637fdc23fc9c
toxinu <toxinu@gmail.com>
2012-06-19 15:45:43 +0200

    Add msgpack backend

---
+++ sofart/backends/_msgpack.py
@@ -0,0 +1,17 @@
+import msgpack
+
+class Backend(object):
+	def __init__(self, path):
+		self.path = path
+		self.packer = msgpack.Packer()
+		self.unpacker = msgpack.Unpacker(use_list=True)
+
+	def init(self, schema):
+		self.dump(schema)
+
+	def dump(self, dump):
+		open(self.path, 'w').write(self.packer.pack(dump))
+
+	def load(self):
+		self.unpacker.feed(open(self.path, 'r').read())
+		return self.unpacker.unpack()
