0e62007767cadc800743380eaf258542cb0277d3
toxinu <toxinu@gmail.com>
2012-06-19 15:46:28 +0200

    Clean unittests

--- tests/test_sofart.py
+++ tests/test_sofart.py
@@ -7,16 +7,28 @@ import os
 
 
 try:
-	from sofart.backends import _json
+	from sofart import Database
 except:
 	print('You need to install sofart before')
 	sys.exit(1)
 
 from sofart import Database
 
-db_path = './so.fart.db'
-mode = 'single'
+#####################################################################################
+# Configuration
+#####################################################################################
+
+#serializer = 'pickle'
 serializer = 'json'
+#serializer = 'msgpack'
+
+db_path = './so.fart.%s.db' % serializer
+mode = 'single'
+
+clean = True
+
+#####################################################################################
+#####################################################################################
 
 _backend = __import__("sofart.backends._%s" % serializer)
 _backend = sys.modules["sofart.backends._%s" % serializer]
@@ -31,29 +43,22 @@ class EmbTestSuite(TestSetup, unittest.TestCase):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		d.drop_collection('test')
 		d.drop_collection('test2')
-		if 'test' in d.collections:
-			raise Exception('Database not empty (drop failure)')
-		if 'test2' in d.collections:
-			raise Exception('Database not empty (drop failure)')
+		self.assertNotIn('test', d.collections, msg='Database not empty (drop failure)')
+		self.assertNotIn('test2', d.collections, msg='Database not empty (drop failure)')
 		d.close()
 
 		_db = backend.load()
-		if 'test' in _db['index']['collections']:
-			raise Exception('Database not empty (drop failure)')
-		if 'test2' in _db['index']['collections']:
-			raise Exception('Database not empty (drop failure)')
-		if len(_db['index']['ids']) > 0:
-			raise Exception('Ids index is not empty')
+		self.assertNotIn('test', _db['index']['collections'])
+		self.assertNotIn('test2', _db['index']['collections'])
+		self.assertFalse(len(_db['index']['ids']) > 0, msg='Ids index is not empty')
 
 	def test_02_new_collection(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		d.new_collection('test')
-		if not 'test' in d.collections:
-			raise Exception('Collection not created')
+		self.assertIn('test', d.collections, msg='Collection not created')
 		d.close()
 		_db = backend.load()
-		if len(_db['index']['ids']) > 0:
-			raise Exception('Ids index is not empty')
+		self.assertFalse(len(_db['index']['ids']) > 0, msg='Ids index is not empty')
 
 	def test_03_getcollection(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
@@ -71,41 +76,36 @@ class EmbTestSuite(TestSetup, unittest.TestCase):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		c = d.get('test')
 		r = c.find_one()
-		if not r:
-			raise Exception('Enreg not found')
+		self.assertTrue(r, msg='Enreg not found')
 	
 	def test_06_listnoenreg(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		c = d.get('test')
 		r = c.find_one({"artist": "Jambon22"})
-		if r:
-			raise Exception('Enreg found')
+		self.assertFalse(r, msg='Enreg found')
 
 	def test_07_nosensitive(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		c = d.get('test')
 		r = c.find_one({"artist": "JambON"}, case_sensitive=False)
-		if not r:
-			raise Exception('Non-sensitive failed')
+		self.assertTrue(r, msg='Non-sensitive failed')
 
 	def test_08_sensitive(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		c = d.get('test')
 		r = c.find_one({"artist": "JamBOn"}, case_sensitive=True)
-		if r:
-			raise Exception('Sensitive failed')
+		self.assertFalse(r, msg='Sensitive failed')
 
 	def test_09_removeenreg(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
 		c = d.get('test')
 		r = c.find_one({"artist": "Jambon"})['_id']
 		c.remove(r)
-		if c.find_one({"artist": "Jambon"}):
-			raise Exception('Enreg not removed')
+		r = c.find_one({"artist": "Jambon"})
+		self.assertFalse(r, msg='Enreg not removed')
 		d.close()
 		_db = backend.load()
-		if r in _db['index']['ids']:
-			raise Exception('Ids not removed in index')
+		self.assertNotIn(r, _db['index']['ids'], msg='Ids not removed in index')
 
 	def test_10_countcollectionenreg(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
@@ -115,8 +115,7 @@ class EmbTestSuite(TestSetup, unittest.TestCase):
 			"music": "I love jambon"}	
 		c.save(post)
 		c.save(post)
-		if c.total_entries() != 2:
-			raise Exception('Data lose (%s)' % c.total_entries())
+		self.assertEqual(c.total_entries(), 2, msg='Data lose (%s)' % c.total_entries())
 
 	def test_11_countdbenreg(self):
 		d = Database(db_path, mode=mode, serializer=serializer)
@@ -127,11 +126,14 @@ class EmbTestSuite(TestSetup, unittest.TestCase):
 			"music": "I love jambon"}	
 		post2 = { 
 			"artist": "Jambon",
-			"music": "I love jambon"}	
+			"music": "I love jambon2"}	
 		c.save(post)
 		c.save(post2)
-		if d.total_entries() != 4:
-			raise Exception('Data lose (%s)' % d.total_entries())
+		self.assertEqual(d.total_entries(), 4, msg='Data lose (%s)' % d.total_entries())
+
+	def test_99_clean(self):
+		if clean:
+			os.remove(db_path)
 
 if __name__ == '__main__':
     unittest.main()
